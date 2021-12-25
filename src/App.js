6de5b258cc971b80cf4fe4af8c24f615ec575347
danderson <andd@netapp.com>
2021-12-25 13:18:17 +0200

    daniels first commit with some css, and a few things messing around

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Navigation_Main_Home_Link:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Daniel's Section
+        </p>
+        <p>
+          Alla FE ONLY
+        </p>
+        <p>
+          Icons
+        </p>
+        <p>
+          Integrated Resume Download from server
+        </p>
       </header>
+      <body className="App-body">
+       <p>
+         A simple react app by Daniel and Alla
+       </p>
+      </body>
     </div>
   );
 }
