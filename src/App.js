fd2af0c1512b0228c4b3b8edc72842dfcd734955
Alla Anderson <76411309+allamalitska@users.noreply.github.com>
2021-12-26 01:01:11 +0200

    added box and styled (#3)
    Squashed and merged, unlike the other merged.
    
    When this PR is merged, there is only 1 single commit generated.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <NavBar/>
       <body className="App-body">
+        <div class = "desing">
+          fuck my brain!
+        </div>
        <p>
          A simple react app by Daniel and Alla
        </p>
